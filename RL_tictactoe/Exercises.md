ea1febc9aa0d11ee9c8c9d9f4bd8d1bafd8f979a
KimMatt <mk1995x@gmail.com>
2020-04-01 12:12:14 -0400

    Refactor tictactoe code and update Exercices.md

--- RL_tictactoe/Exercises.md
+++ RL_tictactoe/Exercises.md
@@ -22,23 +22,44 @@ A simplified Tic Tac Toe where ties are also considered losses. Implementation u
 
 ## Exercises
 
+
 ### Exercise 1.1: Self-Play
 Suppose, instead of playing against a random opponent, the reinforcement learning algorithm described above played against itself. What do you think would happen in this case? Would it learn a different way of playing?
 
-I would expect an agent to perform better against a random agent after it trains with a learning agent.
+I would expect the self play agent to perform better. I would expect it to learn a more intelligent way of playing and for it to learn faster. It would learn faster because it shares a policy map with its opponent. Thus, both agents would learn from each other's mistakes and wins.
 
 ```
-TRAIN VS DUMMY: agent 1 wins: 0.30496, agent 2 wins: 0.26278, ties: 0.43226
-VS. DUMMY: agent 1 wins: 0.8374, agent 2 wins: 0.138, ties: 0.0246
-TRAIN VS. LEARNING AGENT: agent 1 wins: 0.26078, agent 2 wins: 0.3288, ties: 0.41042
-VS. DUMMY: agent 1 wins: 0.77, agent 2 wins: 0.2214, ties: 0.0086
+TRAIN VS. DUMMY: agent 1 wins: 0.73694, agent 2 wins: 0.22716, ties: 0.0359
+VS. DUMMY: agent 1 wins: 0.5602, agent 2 wins: 0.3292, ties: 0.1106
+TRAIN VS. LEARNING AGENT: agent 1 wins: 0.28776, agent 2 wins: 0.28792, ties: 0.42432
+VS. DUMMY: agent 1 wins: 0.7424, agent 2 wins: 0.2464, ties: 0.0112
+Now with self play
+TRAIN VS. LEARNING AGENT: agent 1 wins: 0.28552, agent 2 wins: 0.27866, ties: 0.43582
+VS. DUMMY: agent 1 wins: 0.793, agent 2 wins: 0.172, ties: 0.035
 ```
 
-Both agents played fully greedy when vs. dummy.
+Both agents played fully greedy when vs. the dummy agent. The agent that trained with self play performed almost 5% better than the agent that trained against an seperate learning agent and more than 20% better than the agent which trained against a random opponent.
 
-Contrary to my hypothesis, however, the agent that trained with a random agent performed better. This makes me think that the agent that trained against another learning agent "overfitted" to learning tactics against more intelligent agents. But when faced with a completely random agent, it did not perform as well as an agent that learned tactics which work against a more random agent.
+Perhaps this is closer to reality. Consider when a human player sees another player win, they may try to copy that winning strategy the next time they play.
 
-This experiment was not self playing.
+### Exercise 1.2: Symmetries
+Many tic-tac-toe positions appear different but are really the same because of symmetries. How might we amend the reinforcement learning algorithm described above to take advantage of this? In what ways would this improve it? Now think again. Suppose the opponent did not take advantage of symmetries. In that case, should we? Is it true, then, that symmetrically equivalent positions should necessarily have the same value?
+
+The policies should map all symmetrical states to the same estimated value when updating them.
+
+This could improve our RL method by allowing agents to learn faster. An agent would not have to relearn the same estimates for two states that would otherwise be the same.
+
+If the opponent does not take advantage of the symmetries, they would learn at a slower rate. Also, if a symmetrical-aware agent learns how to defeat the opponent with one strategy, it can easily defeat it in the same way, but with a symmetrical version. Thus, we should treat the symmetrical states equivalently.
+
+```
+TRAIN VS. LEARNING AGENT: agent 1 wins: 0.28762, agent 2 wins: 0.29042, ties: 0.42196
+VS. DUMMY: agent 1 wins: 0.7108, agent 2 wins: 0.2758, ties: 0.0134
+Now with symmetric awareness
+TRAIN VS. LEARNING AGENT: agent 1 wins: 0.28752, agent 2 wins: 0.27084, ties: 0.44164
+VS. DUMMY: agent 1 wins: 0.7794, agent 2 wins: 0.2156, ties: 0.005
+```
+
+The symmetrically aware agent performed ~5% better than the non symmetrically aware agent.
 
 
 ### Exercise 1.3: Greedy Play
@@ -47,7 +68,7 @@ greedy, that is, it always played the move that brought it to the position that
 it rated the best. Would it learn to play better, or worse, than a nongreedy
 player? What problems might occur?
 
-It would not learn as well as a non greedy player. Especially at the beginning of the learning phase, it would be more beneficial to have a high exploration vs. exploitation value. Once the agent learns a few ways to win, it will stick to those potentially suboptimal moves and not explore and learn potentially optimal moves.
+It would learn to play worse than a non greedy player, especially at the beginning of the learning phase. It's beneficial to have a high exploration vs. exploitation value when initially learning, because the agent really knows nothing at the beginning... Once an exploitive agent learns a few ways to win, it will stick to those potentially suboptimal moves and not explore to learn the actual optimal moves.
 
 ```
 TRAIN VS DUMMY: agent 1 wins: 0.73524, agent 2 wins: 0.22646, ties: 0.0383
@@ -56,6 +77,16 @@ TRAIN VS DUMMY: agent 1 wins: 0.1462, agent 2 wins: 0.28714, ties: 0.56666
 VS. DUMMY: agent 1 wins: 0.725, agent 2 wins: 0.2248, ties: 0.0502
 ```
 
-The first trial was with a fully greedy agent 1 while the second trial was with a fully explorative agent. The results align with my hypothesis so I'm going to stick to my guns and believe my logic is sound :)
+The first trial was with a fully greedy agent while the second trial was with a fully explorative agent. The results align with my hypothesis so I'm going to stick to my guns and believe my logic is sound :)
 
 Both agents played fully greedy when versing the dummy.
+
+###Exercise 1.4: Learning from Exploration 
+Suppose learning updates occurred after all moves, including exploratory moves. If the step-size parameter is appropriately reduced over time, then the state values would converge to a set of probabilities. What are the two sets of probabilities computed when we do, and when we do not, learn from exploratory moves? Assuming that we do continue to make exploratory moves, which set of probabilities might be better to learn? Which would result in more wins?
+
+I'm not sure if this is asking if the learning updates occur after all moves of a game or after each move? Is the expected a
+
+###Exercise 1.5: Other Improvements 
+Can you think of other ways to improve the reinforcement learning player? Can you think of any better way to solve the tic-tac-toe problem as posed?
+
+Currently, ties are treated the same as losses. However, a tie would be considered more of a win than a complete loss. And a good tic tac toe player should be able to tie more often than lose. Treating the tie in a way such that it is slightly better than a loss could improve the reinforcement learning player.
--- RL_tictactoe/tictactoe.py
+++ RL_tictactoe/tictactoe.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
-# TODO: Update game to have loss
+# TODO: Batching
+# TODO: Have policies stored independent of player #
 # GOAL: All wins/ties against dummy
 
 class InvalidMoveException(Exception):
@@ -38,28 +39,25 @@ class TicTacToe:
     game_state = None
     logger = None
     winner = None
+    possible_moves = None
 
 
     def __init__(self, logger):
         self.logger = logger
         self.in_progress = True
         self.game_state = [0 for i in range(0, 9)]
+        self.possible_moves = [0,1,2,3,4,5,6,7,8]
 
 
     @staticmethod
     def winning_state(game_state, player_sign, space):
         # Check if the space + player sign is a winning move in given state
-        columns = [[1, 4, 7], [0, 3, 6], [2, 5, 8]]
-        rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        rows = [[1, 4, 7], [0, 3, 6], [2, 5, 8], # verticals
+                [0, 1, 2], [3, 4, 5], [6, 7, 8], # horizontals
+                [0, 4, 8], [2, 4, 6]] # diagonals
         for row in rows:
             if space in row:
-                row.remove(space)
                 if all([game_state[each] == player_sign for each in row]):
-                    return True
-        for column in columns:
-            if space in column:
-                column.remove(space)
-                if all([game_state[each] == player_sign for each in column]):
                     return True
         return False
 
@@ -68,229 +66,290 @@ class TicTacToe:
         # Plays the given move by player in given space
         # Returns whether the move is a winning move or not.
         if self.game_state[space] == 0:
+            self.possible_moves.remove(space)
             self.game_state[space] = player_n
             if TicTacToe.winning_state(self.game_state, player_n, space):
                 self.in_progress = False
                 self.logger.log_agent_win(player_n)
                 self.winner = player_n
                 return True
+            elif not self.possible_moves:
+                self.in_progress = False
+                self.logger.log_tie()
             return False
         else:
             raise InvalidMoveException
-
-
-    def mark_tie(self):
-        # Mark a tie in the game.
-        self.logger.log_tie()
-        self.in_progress = False
 
 
 class Agent:
 
     player_n = None
     game = None
-
+    # Whether or not this agent treats symmetrical game states identically.
+    symmetric_aware = None
+    mirror = None
     exploration = None
-    exploitation = None
-
-    policy = {}
-    game_logs = []
 
 
-    def __init__(self, player_n, exploration):
+    @staticmethod
+    def mirrored_game_state(game_state):
+        mirrored_game_state = game_state[:]
+        for element, i in enumerate(game_state):
+            if element == 1:
+                game_state[i] = 2
+            elif element == 2:
+                game_state[i] = 1
+        return mirrored_game_state
+
+
+    def __init__(self, player_n, exploration, symmetric_aware=False, mirror=False, dummy=False):
         self.exploration = exploration
-        self.exploitation = 1.0 - exploration
         self.player_n = player_n
+        self.symmetric_aware = symmetric_aware
+        # If the policies read/updated need to be done in a mirror fashion.
+        self.mirror = mirror
+        self.game_logs = []
+        self.policy = {}
+        self.dummy = dummy
+
+
+    def set_policy(self, policy):
+        # In order to allow for shared policies between agents.
+        self.policy = policy
+
+
+    def update_policy(self, game_state, last_move, value):
+        if self.mirror:
+            game_state = Agent.mirrored_game_state(game_state)
+        # Helper functions for symmetric awareness
+        def _swap(i,i_prime, game_state):
+            temp = game_state[i]
+            game_state[i] = game_state[i_prime]
+            game_state[i_prime] = temp
+
+        def _swap_x(game_state):
+            _swap(0,2, game_state)
+            _swap(3,5, game_state)
+            _swap(6,8, game_state)
+            return game_state
+
+        def _swap_y(game_state):
+            _swap(0,6, game_state)
+            _swap(1,7, game_state)
+            _swap(2,8, game_state)
+            return game_state
+
+        def _swap_diagonal(game_state):
+            _swap(0,8, game_state)
+            _swap(3,7, game_state)
+            _swap(1,5, game_state)
+            return game_state
+
+        if self.symmetric_aware:
+            x_swapped_game_state = _swap_x(game_state[:])
+            y_swapped_game_state = _swap_y(game_state[:])
+            diagonal_swapped_game_state = _swap_diagonal(_swap_x(game_state[:]))
+            self.policy[str((x_swapped_game_state, last_move))] = value
+            self.policy[str((y_swapped_game_state, last_move))] = value
+            self.policy[str((diagonal_swapped_game_state, last_move))] = value
+        self.policy[str((game_state, last_move))] = value
 
 
     def set_exploration(self, exploration):
         self.exploration = exploration
-        self.exploitation = 1.0 - exploration
 
 
     def enter(self, game):
         self.game = game
 
 
-    def state_p(self, game_state, space):
+    def state_p(self, game_state, last_move):
+        # TODO: Make this always from the perspective of player 1
+        if self.mirror:
+            game_state = Agent.mirrored_game_state(game_state)
         # return the probability of the game state to win according to policy
-        if self.policy.get(str(game_state)) is None:
-            if TicTacToe.winning_state(game_state, self.player_n, space):
-                self.policy[str(game_state)] = 1.0
-                return 1.0
-            self.policy[str(game_state)] = 0.5
+        state_p_value = self.policy.get(str((game_state, last_move)))
+        if state_p_value is None:
+            self.update_policy(game_state[:], last_move, 0.5)
             return 0.5
-        else:
-            return self.policy.get(str(game_state))
+        return state_p_value
 
 
     def play(self):
-        # state is an array representing the current array mode
-        # Marker is either 1 or 2
-        game_state = self.game.game_state[:]
-        # To contain tuples of (probability, move location, game state)
-        possible_moves = []
-        # Can store this as a list in Game to save computation time
-        for i in range(0, len(game_state)):
-            if game_state[i] == 0:
-                # we have a possible move
-                possible_game_state = game_state[:]
-                possible_game_state[i] = self.player_n
-                possible_moves.append((self.state_p(possible_game_state, i), i, possible_game_state))
-        possible_moves.sort()
+        # Compute the likelihoods for each move
+        possible_move_infos = []
+        for move in self.game.possible_moves:
+            possible_game_state = self.game.game_state[:]
+            possible_game_state[move] = self.player_n
+            # possible_move_infos = [(percentage_chance_to_win, move, game_state_as_a_result)]
+            possible_move_infos.append((self.state_p(possible_game_state, move), move, possible_game_state))
 
-        if not possible_moves:
-            self.game.mark_tie()
+        # Based on our exploration value, see if we explore or exploit
+        explore = np.random.uniform(0, 1) <= self.exploration
+        possible_move_infos.sort()
+
+        # Choose a move
+        target_move = None
+
+        # Calculate how many non optimal moves we have
+        optimal_p = possible_move_infos[-1][0]
+        non_optimal_move_counts = 0
+        for i, _ in enumerate(possible_move_infos):
+            if possible_move_infos[i][0] == optimal_p:
+                break
+            non_optimal_move_counts += 1
+
+        # If we have 0 non optimal moves, then just choose a random out of the optimals
+        if non_optimal_move_counts == 0:
+            target_move = np.random.randint(0, len(possible_move_infos))
+        # If explore choose one of the non optimals
+        elif explore:
+            target_move = np.random.randint(0, non_optimal_move_counts + 1) # +1 because end of range is exclusive
+        # If exploit choose one of the optimals
         else:
-            # With a single geometric sample with the probability of exploration
-            # see if we land "heads" on exploration
-            explore = np.random.geometric(p=self.exploration) == 1
-
-            if explore and len(possible_moves) > 1:
-                # try a new approach
-                p_less_moves = possible_moves[:]
-                max_p = possible_moves[-1][0]
-                range_to_max = 0
-                for i in range(0,len(p_less_moves)):
-                    if possible_moves[i][0] == max_p:
-                        break
-                    range_to_max += 1
-                if range_to_max == 0:
-                    self.game_logs.append(possible_moves[0])
-                    self.game.play_move(self.player_n, possible_moves[0][1])
-                else:
-                    exploration_target = np.random.randint(0, range_to_max)
-                    self.game_logs.append(possible_moves[exploration_target])
-                    self.game.play_move(self.player_n, possible_moves[exploration_target][1])
-            else:
-                # exploitation (also includes 1 choice case)
-                # try the approach with highest probability
-                self.game_logs.append(possible_moves[-1])
-                self.game.play_move(self.player_n, possible_moves[-1][1])
+            target_move = np.random.randint(non_optimal_move_counts, len(possible_move_infos))
+        self.game.play_move(self.player_n, possible_move_infos[target_move][1])
+        self.game_logs.append(possible_move_infos[target_move])
 
 
-    def update_policies(self, alpha):
-        for i in range(len(self.game_logs) -2, -1, -1):
-        # update policies
-        # state probability = state probability + alpha(state probs next - current)
-            self.policy[str(self.game_logs[i][2])] = self.state_p(self.game_logs[i][2],
-                self.game_logs[i][1]) + alpha * (self.state_p(self.game_logs[i+1][2],
-                self.game_logs[i+1][1]) - self.state_p(self.game_logs[i][2],
-                self.game_logs[i][1]))
-        self.game_logs = []
+    def back_propagate_policies(self, alpha, reward):
+        if not self.dummy:
+            # update final move policy value
+            self.update_policy(
+                self.game_logs[-1][2][:], self.game_logs[-1][1], reward)
+            # back propagate policy values
+            for i in range(len(self.game_logs) -2, 0, -1):
+            # state probability = state probability + alpha(state probs next - current)
+                current_p = self.state_p(self.game_logs[i][2], self.game_logs[i][1])
+                next_p = self.state_p(self.game_logs[i+1][2], self.game_logs[i+1][1])
+                update_p = current_p + (alpha * (next_p - current_p))
+                self.update_policy(self.game_logs[i][2][:], self.game_logs[i][1], update_p)
+            # Clear game logs in case we want to play another game with this agent
+            self.game_logs = []
 
 
 if __name__ == "__main__":
 
+    def backprop_agents(game_winner, a1, a2, alpha):
+        # set reward values based on game outcome
+        if game_winner is None:
+            a1_reward = 0.5
+            a2_reward = 0.5
+        else:
+            a1_reward = 1.0 if game_winner == 1 else 0.0
+            a2_reward = 1.0 if game_winner == 2 else 0.0
+        # update policies
+        a1.back_propagate_policies(alpha, a1_reward)
+        a2.back_propagate_policies(alpha, a2_reward)
 
-    def intelligent_play(agent1, agent2):
-        iterations = 50000
-        games_count = iterations
-        # Hyperparameters
-        alpha = 0.5
-        decrease_factor = 0.99
-        logger = Logger()
+    def run_games(iterations, a1, a2, logger, **kwargs):
+        """
+        Args:
+            a1 ([Agent]): agent 1 object to play
+            a2 ([Agent]): agent 2 object to be played against
+            iterations ([int]): number of games to run
+            logger ([Logger]): logger object to keep track of wins/losses/ties
+            kwargs:
+                training ([Boolean]): whether or not this is a training run
+                    if true then will backpropagate
+                alpha ([Float]): hyperparameter for update values
+                decrease_factor ([Float]): hyperparameter for alpha decrease over time
+        """
+        if kwargs.get('training'):
+            training = kwargs.get('training')
+            alpha = kwargs.get('alpha') if kwargs.get('alpha') is not None else 0.2
+            decrease_factor = kwargs.get('decrease_factor') if kwargs.get('decrease_factor') is None else 0.9
+        else:
+            training = False
 
-        while iterations > 0:
+        games_left = iterations
+        while games_left > 0:
             game = TicTacToe(logger)
-            agent1.enter(game)
-            agent2.enter(game)
+            a1.enter(game)
+            a2.enter(game)
+            a1_turn = True if games_left % 2 == 0 else False
             while game.in_progress:
-                if iterations % 2 == 0:
-                    # play game
-                    agent1.play()
-                    if game.in_progress:
-                        agent2.play()
+                if a1_turn:
+                    a1.play()
                 else:
-                    agent2.play()
-                    if game.in_progress:
-                        agent1.play()
-            # update policies
-            agent1.update_policies(alpha)
-            agent2.update_policies(alpha)
-            # Decrease alpha over time
-            if iterations % (math.ceil(iterations/50)) == 0:
-                alpha = alpha * decrease_factor
-            iterations -= 1
-        print("TRAIN VS. LEARNING AGENT: agent 1 wins: {}, agent 2 wins: {}, ties: {}".format(
-            (logger.agent_1_wins / games_count), (logger.agent_2_wins / games_count),
-            (logger.ties / games_count)))
+                    a2.play()
+                a1_turn = not a1_turn
+            if training:
+                backprop_agents(game.winner, a1, a2, alpha)
+                # Decrease alpha over time
+                if games_left % (math.ceil(games_left/50)) == 0:
+                    alpha = alpha * decrease_factor
+            games_left -= 1
 
+
+    def train(iterations, a1, a2):
+        # Hyperparameters
+        alpha = 0.2
+        decrease_factor = 0.9
+        logger = Logger()
+        run_games(iterations, a1, a2, logger, training=True,
+                  alpha=alpha, decrease_factor=decrease_factor)
+        if a2.dummy:
+            print("TRAIN VS. DUMMY: agent 1 wins: {}, agent 2 wins: {}, ties: {}".format(
+                (logger.agent_1_wins / iterations), (logger.agent_2_wins / iterations),
+                (logger.ties / iterations)))
+        else:
+            print("TRAIN VS. LEARNING AGENT: agent 1 wins: {}, agent 2 wins: {}, ties: {}".format(
+                (logger.agent_1_wins / iterations), (logger.agent_2_wins / iterations),
+                (logger.ties / iterations)))
+        # returns the better agent
         if logger.agent_1_wins > logger.agent_2_wins:
-            return agent1
-        return agent2
+            return a1
+        return a2
 
 
-    def train_vs_dummy(agent1, agent2):
-        iterations = 50000
-        games_count = iterations
-        # Hyperparameters
-        alpha = 0.5
-        decrease_factor = 0.99
+    def test(iterations, a1, a2):
         logger = Logger()
+        run_games(iterations, a1, a2, logger)
+        print("TEST VS. DUMMY: agent 1 wins: {}, agent 2 wins: {}, ties: {}".format( (
+            logger.agent_1_wins / iterations ), ( logger.agent_2_wins / iterations ),
+            (logger.ties / iterations)))
 
-        while iterations > 0:
-            game = TicTacToe(logger)
-            agent1.enter(game)
-            agent2.enter(game)
-            while game.in_progress:
-                if iterations % 2 == 0:
-                    # play game
-                    agent1.play()
-                    if game.in_progress:
-                        agent2.play()
-                else:
-                    agent2.play()
-                    if game.in_progress:
-                        agent1.play()
-            # update policies
-            agent1.update_policies(alpha)
-            # Decrease alpha over time
-            if iterations % (math.ceil(iterations/50)) == 0:
-                alpha = alpha * decrease_factor
-            iterations -= 1
-        print("TRAIN VS DUMMY: agent 1 wins: {}, agent 2 wins: {}, ties: {}".format(
-            (logger.agent_1_wins / games_count), (logger.agent_2_wins / games_count),
-            (logger.ties / games_count)))
-
-        return agent1
-
-
-    def dummy_tournament(agent1, agent2):
-        iterations = 5000
-        games_count = iterations
-        logger = Logger()
-
-        while iterations > 0:
-            game = TicTacToe(logger)
-            agent1.enter(game)
-            agent2.enter(game)
-            while game.in_progress:
-                if iterations % 2 == 0:
-                    # play game
-                    agent1.play()
-                    if game.in_progress:
-                        agent2.play()
-                else:
-                    agent2.play()
-                    if game.in_progress:
-                        agent1.play()
-            iterations -= 1
-        print("VS. DUMMY: agent 1 wins: {}, agent 2 wins: {}, ties: {}".format( (
-            logger.agent_1_wins / games_count ), ( logger.agent_2_wins / games_count ),
-            ( logger.ties / games_count )))
-
-    AGENT_1 = Agent(1, 0)
-    AGENT_2 = Agent(2, 0.8)
-    train_vs_dummy(AGENT_1, AGENT_2)
+    AGENT_1 = Agent(1, 0.8)
+    AGENT_2 = Agent(2, 0.8, dummy=True)
+    train(50000, AGENT_1, AGENT_2)
     AGENT_1.set_exploration(0)
-    AGENT_3 = Agent(2, 0.5)
-    dummy_tournament(AGENT_1, AGENT_3)
+    AGENT_3 = Agent(2, 0.5, dummy=True)
+    test(50000, AGENT_1, AGENT_3)
 
     AGENT_1 = Agent(1, 0.8)
     AGENT_2 = Agent(2, 0.8)
-    WINNER = intelligent_play(AGENT_1, AGENT_2)
+    WINNER = train(50000, AGENT_1, AGENT_2)
     WINNER.player_n = 1
     WINNER.set_exploration(0)
-    AGENT_3 = Agent(2, 0.5)
-    dummy_tournament(WINNER, AGENT_3)
+    AGENT_3 = Agent(2, 0.5, dummy=True)
+    test(50000, WINNER, AGENT_3)
+
+    print("Now with symmetric awareness")
+    AGENT_1 = Agent(1, 0.8, symmetric_aware=True)
+    AGENT_2 = Agent(2, 0.8)
+    WINNER = train(50000, AGENT_1, AGENT_2)
+    WINNER.player_n = 1
+    WINNER.set_exploration(0)
+    AGENT_3 = Agent(2, 0.5, dummy=True)
+    test(50000, WINNER, AGENT_3)
+
+    print("Now with self play")
+    AGENT_1 = Agent(1, 0.8)
+    AGENT_2 = Agent(2, 0.8, mirror=True)
+    AGENT_2.set_policy(AGENT_1.policy)
+    WINNER = train(50000, AGENT_1, AGENT_2)
+    WINNER.player_n = 1
+    WINNER.set_exploration(0)
+    AGENT_3 = Agent(2, 0.5, dummy=True)
+    test(50000, WINNER, AGENT_3)
+
+    print("Now with self play & symmetric awareness")
+    AGENT_1 = Agent(1, 0.8)
+    AGENT_2 = Agent(2, 0.8)
+    AGENT_2.set_policy(AGENT_1.policy)
+    WINNER = train(50000, AGENT_1, AGENT_2)
+    WINNER.player_n = 1
+    WINNER.set_exploration(0)
+    AGENT_3 = Agent(2, 0.5, dummy=True)
+    test(50000, WINNER, AGENT_3)
