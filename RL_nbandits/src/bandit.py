851e2d5ab2861c4e56ebb7377f1b91307603c936
KimMatt <mk1995x@gmail.com>
2020-04-11 17:07:56 -0400

    starting files

---
+++ RL_nbandits/src/bandit.py
@@ -0,0 +1,19 @@
+# bandit.py
+# class to interface with for a bandit AKA a randomly distributed 'slot machine'
+
+class Distribution:
+
+    def __init__(self):
+        pass
+
+    def sample(self):
+        pass
+
+
+class Bandit:
+
+    def __init__(self):
+        pass
+
+    def check(self):
+        pass
