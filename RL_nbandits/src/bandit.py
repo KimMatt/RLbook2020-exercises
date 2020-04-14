159dc461690fbc6c40777ea98c68eaa48e76921f
KimMatt <mk1995x@gmail.com>
2020-04-13 21:43:26 -0400

    Flesh out agent and bandit

--- RL_nbandits/src/bandit.py
+++ RL_nbandits/src/bandit.py
@@ -1,19 +1,47 @@
 # bandit.py
 # class to interface with for a bandit AKA a randomly distributed 'slot machine'
 
-class Distribution:
+import numpy as np
+
+class IncompleteBanditException(Exception):
 
     def __init__(self):
-        pass
+        super().__init__()
+
+
+class Distribution:
+
+    def __init__(self, chance):
+        self.chance = chance
 
     def sample(self):
         pass
 
 
+class UniformDistribution(Distribution):
+
+    def __init__(self, chance, mean, std):
+        super().__init__(chance)
+        self.std = std
+        self.mean = mean
+
+    def sample(self):
+        return np.random.normal(loc=self.mean, scale=self.std)
+
+
 class Bandit:
 
-    def __init__(self):
-        pass
+    def __init__(self, n):
+        self.distributions = [[] for i in range(n)]
 
-    def check(self):
-        pass
+    def sample(self, n):
+        which = np.random.uniform(0, 1)
+        cumulative_chance = 0.0
+        for distribution in self.distributions[n]:
+            if which <= distribution.chance + cumulative_chance:
+                return distribution.sample()
+            cumulative_chance += distribution.chance
+        raise IncompleteBanditException
+
+    def add_distribution(self, distribution, n):
+        self.distributions.append(distribution)
