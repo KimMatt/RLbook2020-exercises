80e779fb5b13dffe73da036d447922853cbc7e85
root <root@DESKTOP-HVEDC24.localdomain>
2020-08-06 22:02:29 -0400

    Add ucb implementation to ch 2

--- Ch_2_NBandits/src/agent.py
+++ Ch_2_NBandits/src/agent.py
@@ -10,12 +10,13 @@ class Agent:
     accepted_types = {"means": True, "constant": True, "increasing": True}
 
     def __init__(self, exploration, val_init, bandit, **kwargs):
-        """[summary]
-
+        """
         Args:
             exploration ([float]): value between 0 and 1 of how much to explore vs. exploit
             val_init ([float]): value to initialize all the policies to
             bandit ([Object]): bandit object to interface with
+            ucb (boolean): whether or not to use ucb exploraton parameter, default
+            is epsilon greedy
             kwargs:
                 method ([string], optional): method of updating policies
                                             can be 'means', 'constant', or 'increasing'
@@ -32,6 +33,12 @@ class Agent:
         self.k_tracker = [1.0 for i in range(n)]
 
         self.method = kwargs.get("method") if kwargs.get("method") else "means"
+        self.ucb = kwargs.get("ucb")
+        if self.ucb is True:
+            self.t = 0
+            self.c = 2
+            self.bandit_visits = [0 for i in range(self.bandit.n)]
+
         if not self.accepted_types.get(self.method):
             raise Exception
         self.constant = kwargs.get("constant") if kwargs.get("constant") else 0.1
@@ -48,13 +55,21 @@ class Agent:
         self.policy[arm] += float(math.tanh(self.k_tracker[arm]) * self.constant * (reward - self.policy[arm]))
 
     def play(self):
-        explore = np.random.uniform(0, 1) <= self.exploration
-        max_value = np.max(np.array(self.policy))
-        if explore:
-            arm = np.random.randint(self.bandit.n)
+        # select arm either based on epsilon greedy policy or ucb
+        if self.ucb is not True:
+            roll = np.random.rand()
+            explore = roll <= self.exploration
+            if explore:
+                arm = np.random.randint(self.bandit.n)
+            else:
+                max_value = np.max(np.array(self.policy))
+                arm = self.policy.index(max_value)
         else:
-            max_value = np.max(np.array(self.policy))
-            arm = self.policy.index(max_value)
+            ucbs = [(self.policy[arm] + (math.sqrt(math.log(self.t)/self.bandit_visits[arm]) * self.c)) for arm in range(self.bandits.n)]
+            arm = ucbs.index(max(ucbs))
+            self.t += 1
+            self.bandit_visits[arm] += 1
+        # get reward and update reward means
         reward = self.bandit.sample(arm)
         if self.method == "means":
             self.update_reward_means_method(arm, reward)
